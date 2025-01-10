2c2ed091dfc73d742005b693c547aebdd9016987
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2025-01-10 15:36:38 +0100

    feat: add the usle of images in asynStorage

--- hooks/forms/useTakePicture.ts
+++ hooks/forms/useTakePicture.ts
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 import { CameraType, useCameraPermissions } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function useTakePicture() {
 
     const [facing, setFacing] = useState<CameraType>('back');
     const [permission, requestPermission] = useCameraPermissions();
-    const [image, setImage] = useState<string | null>(null);
 
     const takePicture = async (cameraRef: any) => {
         if (cameraRef) {
             const imageData = await cameraRef.takePictureAsync();
-            console.log(imageData);
-            setImage(imageData.uri);
+            await saveImageUri(imageData.uri);
+        }
+    };
+
+    const saveImageUri = async (uri: string) => {
+        try {
+          const existingUris = await AsyncStorage.getItem('capturedImageUris');
+          const uris = existingUris ? JSON.parse(existingUris) : [];
+      
+          uris.push(uri);
+      
+          await AsyncStorage.setItem('capturedImageUris', JSON.stringify(uris));
+          console.log('Image URI saved:', uri);
+        } catch (error) {
+          console.error('Error saving image URI:', error);
         }
     };
 
